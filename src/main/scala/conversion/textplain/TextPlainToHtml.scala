c8d5944a29af3209b473164a17a5571a609c92c3
Neftales Antunes <neftales@gmail.com>
2015-09-07 20:48:24 -0300

    Implementação inicial da conversão de TXT -> HTML.

--- src/main/scala/conversion/textplain/TextPlainToHtml.scala
+++ src/main/scala/conversion/textplain/TextPlainToHtml.scala
@@ -1,6 +1,21 @@
 package conversion.textplain
 
-case object TextPlainToHtml extends Function[Seq[Byte], Seq[Byte]]{
-  def apply(file: Seq[Byte]): Seq[Byte] = ???
+import java.io.File
+
+import com.gilt.handlebars.scala.Handlebars
+import com.gilt.handlebars.scala.binding.dynamic._
+
+case object TextPlainToHtml extends Function[Seq[Byte], Seq[Byte]] {
+  def apply(file: Seq[Byte]): Seq[Byte] = {
+    val path = getClass.getClassLoader.getResource("templateHTML.html").getPath
+    val template = new File(path)
+    val t = Handlebars(template)
+    val map = Map("body" -> readSeq(file))
+    val textoSaida = t(map)
+
+    textoSaida.getBytes.toSeq
+  }
+
+
 }
 
