50ec6980431fe892e21b8afa4918ff4bd7ab29c5
Neftales Antunes <neftales@gmail.com>
2015-09-07 20:47:34 -0300

    Função para ler um Arquivo e retornar seu conteúdo em String.

---
+++ src/main/scala/conversion/textplain/package.scala
@@ -0,0 +1,13 @@
+package conversion
+
+import java.io.ByteArrayInputStream
+
+package object textplain {
+
+  def readSeq(seq: Seq[Byte]): String = {
+    val byteArray = seq.toArray
+    val file = new ByteArrayInputStream(byteArray)
+    file.read
+    new String(byteArray)
+  }
+}
