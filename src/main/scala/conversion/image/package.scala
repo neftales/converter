63e3d54df763b1252a704cfbfe1c8fe78f177fcf
Neftales Antunes <neftales@gmail.com>
2015-09-16 21:34:01 -0300

    Trocando var por val.

--- src/main/scala/conversion/image/package.scala
+++ src/main/scala/conversion/image/package.scala
@@ -7,7 +7,7 @@ import javax.imageio.ImageIO
 
 package object image {
   def read(bytes: Seq[Byte]): BufferedImage = {
-    var arrayBytes = bytes.toArray
+    val arrayBytes = bytes.toArray
     val stream = new ByteArrayInputStream(arrayBytes)
 
     ImageIO.read(stream)
@@ -15,10 +15,9 @@ package object image {
 
   def writeTo(file: Seq[Byte], format: String): Seq[Byte] = {
     val img = read(file)
-    var newImg = new BufferedImage(img.getWidth, img.getHeight, BufferedImage.TYPE_INT_RGB)
+    val newImg = new BufferedImage(img.getWidth, img.getHeight, BufferedImage.TYPE_INT_RGB)
     newImg.createGraphics().drawImage(img, 0, 0, Color.WHITE, null)
-    var arrayOutputStream = new ByteArrayOutputStream()
-
+    val arrayOutputStream = new ByteArrayOutputStream()
 
     ImageIO.write(newImg, format, arrayOutputStream)
     arrayOutputStream.toByteArray.toSeq
