8bb84d42c67c13b9357e654110c8e4cd5761537e
Neftales Antunes <neftales@gmail.com>
2015-09-16 21:22:19 -0300

    Implementando conversões entre alguns tipos comuns de imagens.
    Foi adicionado funções para converter JPEG, GIF, PNG, BMP. Há conversões
    para todas as combinações de entre os formatos esses formatos.

---
+++ src/main/scala/conversion/image/Image.scala
@@ -0,0 +1,29 @@
+package conversion.image
+
+object Image {
+
+  object toBMP extends Function[Seq[Byte], Seq[Byte]] {
+    override def apply(file: Seq[Byte]): Seq[Byte] = {
+      writeTo(file, "bmp")
+    }
+  }
+
+  object toGIF extends Function[Seq[Byte], Seq[Byte]] {
+    override def apply(file: Seq[Byte]): Seq[Byte] = {
+      writeTo(file, "gif")
+    }
+  }
+
+  object toJPEG extends Function[Seq[Byte], Seq[Byte]] {
+    override def apply(file: Seq[Byte]): Seq[Byte] = {
+      writeTo(file, "jpg")
+    }
+  }
+
+  object toPNG extends Function[Seq[Byte], Seq[Byte]] {
+    override def apply(file: Seq[Byte]): Seq[Byte] = {
+      writeTo(file, "png")
+    }
+  }
+
+}
