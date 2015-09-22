ac2fc13844f4d97ad01d7ac18bef079315d23de3
Neftales Antunes <neftales@gmail.com>
2015-09-22 19:17:53 -0300

    Main com graph atualizado.

--- src/main/scala/Applications.scala
+++ src/main/scala/Applications.scala
@@ -1,12 +1,24 @@
-import graph.ConvertGraph
+
+import conversion.image.Image
+import conversion.textplain.TextPlainToHtml
+import graph.{ConvertGraph, Dijkstra}
+
 
 object Applications extends App {
   // 1. Construct graph
-  val g = new ConvertGraph
-  val textPlain = g.addNode("TXT")
-  val html = g.addNode("HTML")
-  val pdf = g.addNode("PDF")
+  val graph = new ConvertGraph
+  // Starting graph with some nodes
+  graph(List("TXT", "HTML", "PNG", "TIFF", "GIF", "BMP", "JPEG"))
 
-  println(g.nodes)
+  graph connectWithWeightWithBehavior("TXT", List(("HTML", 1, TextPlainToHtml)))
+  graph connectWithWeightWithBehavior("PNG", List(("TIFF", 5, Image.toTIFF), ("JPEG", 1, Image.toJPEG)))
+  graph connectWithWeightWithBehavior("JPEG", List(("BMP", 5, Image.toBMP)))
+  graph connectWithWeightWithBehavior("BMP", List(("GIF", 3, Image.toGIF)))
+  graph connectWithWeightWithBehavior("GIF", List(("PNG", 1, Image.toPNG)))
+
+  val dijkstra = new Dijkstra[graph.type](graph)
+  val list = dijkstra.mountListOfEdge(graph.getNode("PNG"), graph.getNode("GIF"))
+
+  list foreach (x => println(x))
 }
 
