6e27c7797f69b7c7e84a287385d403826c8e4eb9
Neftales Antunes <neftales@gmail.com>
2015-09-25 16:16:25 -0300

    Clean no main.

--- src/main/scala/Applications.scala
+++ src/main/scala/Applications.scala
@@ -1,7 +1,7 @@
 
 import conversion.image.Image
 import conversion.textplain.TextPlainToHtml
-import graph.{ConvertGraph, Dijkstra}
+import graph.ConvertGraph
 
 
 object Applications extends App {
@@ -10,15 +10,10 @@ object Applications extends App {
   // Starting graph with some nodes
   graph(List("TXT", "HTML", "PNG", "TIFF", "GIF", "BMP", "JPEG"))
 
-  graph connectWithWeightWithBehavior("TXT", List(("HTML", 1, TextPlainToHtml)))
-  graph connectWithWeightWithBehavior("PNG", List(("TIFF", 5, Image.toTIFF), ("JPEG", 1, Image.toJPEG)))
-  graph connectWithWeightWithBehavior("JPEG", List(("BMP", 5, Image.toBMP)))
-  graph connectWithWeightWithBehavior("BMP", List(("GIF", 3, Image.toGIF)))
-  graph connectWithWeightWithBehavior("GIF", List(("PNG", 1, Image.toPNG)))
-
-  val dijkstra = new Dijkstra[graph.type](graph)
-  val list = dijkstra.mountListOfEdge(graph.getNode("PNG"), graph.getNode("GIF"))
-
-  list foreach (x => println(x))
+  graph.connectWithWeightWithBehavior("TXT", List(("HTML", 1, TextPlainToHtml)))
+  graph.connectWithWeightWithBehavior("PNG", List(("TIFF", 5, Image.toTIFF), ("JPEG", 1, Image.toJPEG)))
+  graph.connectWithWeightWithBehavior("JPEG", List(("BMP", 5, Image.toBMP)))
+  graph.connectWithWeightWithBehavior("BMP", List(("GIF", 3, Image.toGIF)))
+  graph.connectWithWeightWithBehavior("GIF", List(("PNG", 1, Image.toPNG)))
 }
 
