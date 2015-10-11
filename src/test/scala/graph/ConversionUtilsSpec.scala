0d9d2c64005b0387c5a0abd7130a2418cd7efd74
Neftales Antunes <neftales@gmail.com>
2015-10-11 10:54:56 -0300

    Removendo declaração do grafo no teste

--- src/test/scala/graph/ConversionUtilsSpec.scala
+++ src/test/scala/graph/ConversionUtilsSpec.scala
@@ -1,22 +1,12 @@
 package graph
 
-import java.io.{FileOutputStream, File, FileInputStream}
+import java.io.{File, FileInputStream}
 
-import conversion.image.Image
-import conversion.textplain.TextPlainToHtml
 import org.scalatest.{FlatSpec, Matchers}
 
 class ConversionUtilsSpec extends FlatSpec with Matchers {
 
   val filePNG = new File(getClass.getClassLoader.getResource("docker.png").getPath)
-
-  val graph = new ConvertGraph
-  graph(List("TXT", "HTML", "PNG", "TIFF", "GIF", "BMP", "JPEG"))
-  graph connectWithWeightWithBehavior("TXT", List(("HTML", 1, TextPlainToHtml)))
-  graph connectWithWeightWithBehavior("PNG", List(("TIFF", 5, Image.toTIFF), ("JPEG", 1, Image.toJPEG)))
-  graph connectWithWeightWithBehavior("JPEG", List(("BMP", 5, Image.toBMP)))
-  graph connectWithWeightWithBehavior("BMP", List(("GIF", 3, Image.toGIF)))
-  graph connectWithWeightWithBehavior("GIF", List(("PNG", 1, Image.toPNG)))
 
   "PNG" should "be conveted in GIF file" in {
     val conversion = ConversionUtils.getConversion("PNG", "GIF")
