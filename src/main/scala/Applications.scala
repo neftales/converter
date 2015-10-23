13a014ddc9c618c884903b2d987f5facc10c3a70
Neftales Antunes <neftales@gmail.com>
2015-10-23 15:04:34 -0200

    Server scalatra.

--- src/main/scala/Applications.scala
+++ src/main/scala/Applications.scala
@@ -1,8 +1,27 @@
 import com.typesafe.scalalogging.slf4j.LazyLogging
 
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.DefaultServlet
+import org.eclipse.jetty.webapp.WebAppContext
+import org.scalatra.servlet.ScalatraListener
 object Applications extends App with LazyLogging {
 
-  logger.info("Serviço iniciado.")
+  val port = Option(System.getenv("PORT")) match {
+    case Some(p) => p.toInt
+    case None    => 8080
+  }
 
+  val server = new Server(port)
+  val context = new WebAppContext()
+
+  context setContextPath "/"
+  context.setResourceBase("src/main/webapp")
+  context.addEventListener(new ScalatraListener)
+  context.addServlet(classOf[DefaultServlet], "/")
+
+  server.setHandler(context)
+
+  server.start()
+  server.join()
 }
 
