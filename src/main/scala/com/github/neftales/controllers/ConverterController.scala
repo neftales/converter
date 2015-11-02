5816225cbbb17a267a19220821db60912b3b59de
Neftales Antunes <neftales@gmail.com>
2015-11-02 17:45:27 -0200

    Endopoint para mostrar os formatos suportados pelo conversor.

--- src/main/scala/com/github/neftales/controllers/ConverterController.scala
+++ src/main/scala/com/github/neftales/controllers/ConverterController.scala
@@ -1,19 +1,12 @@
 package com.github.neftales.controllers
 
 
-import java.net.InetAddress
-
 import com.github.neftales.api.ConversionUtils
 import com.typesafe.scalalogging.slf4j.LazyLogging
 
-/**
- * Created by Neftales on 01/11/2015.
- */
 class ConverterController extends BaseController with LazyLogging {
 
-  get("/") {
-    val host = InetAddress.getLocalHost.getHostName
-    logger.info(host)
+  get("/nodes/") {
     ConversionUtils.getNodes()
   }
 
