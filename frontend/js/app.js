c06f77bdf20fe5c8a5e0bb6c24f034465bd19110
Neftales Antunes <neftales@gmail.com>
2015-11-22 14:00:41 -0200

    Removendo frontend.

--- frontend/js/app.js
+++
@@ -1,9 +0,0 @@
-var React = require("react");
-var FileForm = require("./components/file-form.jsx");
-var Actions = require("./actions/actions.js");
-
-function app() {
-  React.render(<FileForm/>, document.getElementById('content'));
-}
-
-window.onload = app;
