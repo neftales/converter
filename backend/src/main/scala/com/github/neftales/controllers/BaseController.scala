4247976ec45636440c9e497d3b21de1f5b7e8a44
Neftales Antunes <neftales@gmail.com>
2015-11-22 13:54:40 -0200

    add suporte ao Cors

--- backend/src/main/scala/com/github/neftales/controllers/BaseController.scala
+++ backend/src/main/scala/com/github/neftales/controllers/BaseController.scala
@@ -4,14 +4,15 @@ import com.github.neftales.applicationExecutor
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.json.JacksonJsonSupport
-import org.scalatra.{FutureSupport, ScalatraServlet}
+import org.scalatra.{CorsSupport, FutureSupport, ScalatraServlet}
 import scala.concurrent.ExecutionContext
 import com.github.neftales.api.ConversionFormats
 
 trait BaseController extends ScalatraServlet
 with JacksonJsonSupport
 with FutureSupport
-with LazyLogging {
+with LazyLogging
+with CorsSupport {
 
   protected implicit lazy val jsonFormats: Formats = ConversionFormats
   protected implicit val executor: ExecutionContext = applicationExecutor
