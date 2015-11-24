4cd4ebc9c2414848157fdd33f4790da49b6e127b
Neftales Antunes <neftales@gmail.com>
2015-11-24 19:56:28 -0200

    Fix template.

--- backend/src/main/scala/com/github/neftales/conversion/textplain/TextPlainToHtml.scala
+++ backend/src/main/scala/com/github/neftales/conversion/textplain/TextPlainToHtml.scala
@@ -1,22 +1,30 @@
 package com.github.neftales.conversion.textplain
 
-import java.io.File
-
 import com.gilt.handlebars.scala.Handlebars
 import com.gilt.handlebars.scala.binding.dynamic._
 import com.typesafe.scalalogging.slf4j.LazyLogging
 
 case object TextPlainToHtml extends Function[Seq[Byte], Seq[Byte]] with LazyLogging {
+
+  val template =
+    """<!DOCTYPE html>
+    |<html lang="">
+    | <head>
+    |   <meta charset="UTF-8">
+    |   <title></title>
+    | </head>
+    | <body>
+    |   {{& body}}
+    | </body>
+    |</html>""".stripMargin
+
   def apply(file: Seq[Byte]): Seq[Byte] = {
+      val t = Handlebars(template)
+      val map = Map("body" -> readSeq(file).replace("\n", "<br>"))
+      val textoSaida = t(map)
 
-    val path = getClass.getClassLoader.getResource("templateHTML.html").getPath
-    val template = new File(path)
-    val t = Handlebars(template)
-    val map = Map("body" -> readSeq(file).replace("\n", "<br>"))
-    val textoSaida = t(map)
-
-    logger.info(s" -> HTML")
-    textoSaida.getBytes.toSeq
+      logger.info(s" -> HTML")
+      textoSaida.getBytes.toSeq
   }
 
 }
