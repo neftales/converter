f1cf5d31a3616e0e6928f376e4c4178c22e3fc9b
Neftales Antunes <neftales@gmail.com>
2016-04-03 11:50:14 -0300

    Removendo do grafo formatos não implementandos ainda.

--- backend/src/main/scala/com/github/neftales/graph/package.scala
+++ backend/src/main/scala/com/github/neftales/graph/package.scala
@@ -6,9 +6,8 @@ import com.github.neftales.conversion.textplain.TextPlainToHtml
 package object graph {
 
   implicit val converteGraph = new ConvertGraph
-  converteGraph(List("TXT", "HTML", "PNG", "TIFF", "GIF", "BMP", "JPEG"))
+  converteGraph(List("PNG", "TIFF", "GIF", "BMP", "JPEG"))
 
-  converteGraph.connectWithWeightWithBehavior("TXT", List(("HTML", 1, TextPlainToHtml)))
   converteGraph.connectWithWeightWithBehavior("PNG", List(("TIFF", 5, Image.toTIFF), ("JPEG", 1, Image.toJPEG)))
   converteGraph.connectWithWeightWithBehavior("JPEG", List(("BMP", 5, Image.toBMP)))
   converteGraph.connectWithWeightWithBehavior("BMP", List(("GIF", 3, Image.toGIF)))
